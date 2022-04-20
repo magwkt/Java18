a6a126ad76d42cc9bf234e45e641cff596dd8700
Magdalena <krystynapawlowicz@wp.pl>
2022-04-20 17:52:41 +0200

    commit Java18.java

---
+++ src/Java18.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Java18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        if (age >18) {
+            System.out.println("jest super!");
+        } else {
+            System.out.println("jest lipa!");
+        }
+    }
+}
